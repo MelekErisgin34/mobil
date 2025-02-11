764f3df74dc8e5b71d10399e6f020bcb3fda3c4f
MelekErisgin34 <147922709+MelekErisgin34@users.noreply.github.com>
2025-02-11 13:00:40 +0300

    Add files via upload

---
+++ java/com/example/uygulama1/Ders.java
@@ -0,0 +1,54 @@
+package com.example.uygulama1;
+
+public class Ders {
+    String dersAdi;
+    int not1;
+    int not2;
+    double ortalama;
+    String durum;
+
+    public String getDersAdi() {
+        return dersAdi;
+    }
+
+    public void setDersAdi(String dersAdi) {
+        this.dersAdi = dersAdi;
+    }
+
+    public int getNot1() {
+        return not1;
+    }
+
+    public void setNot1(int not1) {
+        this.not1 = not1;
+    }
+
+    public int getNot2() {
+        return not2;
+    }
+
+    public void setNot2(int not2) {
+        this.not2 = not2;
+    }
+
+    public double getOrtalama() {
+        return ortalama;
+    }
+
+    public void setOrtalama(double ortalama) {
+        this.ortalama = ortalama;
+    }
+
+    public String getDurum() {
+        return durum;
+    }
+
+    public void setDurum(String durum) {
+        this.durum = durum;
+    }
+    public double ortalamahesaplama(){
+
+    }
+
+}
+
